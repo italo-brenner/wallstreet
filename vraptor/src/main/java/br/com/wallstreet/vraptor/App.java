8d87d8f1bf5f69389a82d615b42daf1aea87a926
Ítalo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2016-11-08 17:05:41 -0300

    Primeiro Commit

---
+++ vraptor/src/main/java/br/com/wallstreet/vraptor/App.java
@@ -0,0 +1,13 @@
+package br.com.wallstreet.vraptor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
