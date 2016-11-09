be83f8cdf2a0cb286f8536e30441a71695a2f108
Ítalo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2016-11-09 11:20:26 -0300

    Criar requisições das páginas

--- vraptor/src/main/resources/import.sql
+++ vraptor/src/main/resources/import.sql
@@ -1,8 +1,12 @@
 insert into Acao (nomeEmpresa, valor) values ('A Suprema', 10.00);
+insert into Acao (nomeEmpresa, valor) values ('NOVIMODAS', 100.00);
 insert into Acao (nomeEmpresa, valor) values ('GBarbosa', 20.00);
 insert into Acao (nomeEmpresa, valor) values ('Gerdau', 30.00);
-insert into Acao (nomeEmpresa, valor) values ('Bompreço', 40.00);
-insert into Acao (nomeEmpresa, valor) values ('Petrobrás', 50.00);
-
+insert into Acao (nomeEmpresa, valor) values ('COSIL', 60.00);
+insert into Acao (nomeEmpresa, valor) values ('Bompreco', 40.00);
+insert into Acao (nomeEmpresa, valor) values ('PNEUSERVICE', 90.00);
+insert into Acao (nomeEmpresa, valor) values ('Petrobras', 50.00);
+insert into Acao (nomeEmpresa, valor) values ('CICLOPECAS', 70.00);
+insert into Acao (nomeEmpresa, valor) values ('PANIFICACAO GARCA', 80.00);
 
 
